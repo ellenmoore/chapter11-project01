8b1831001a6b93661f1da778508cd1604427f0cb
Ellen Moore <ellenmoore13@gmail.com>
2016-12-01 21:23:31 -0600

    Add files via upload
    add config file for access to database

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+define('DBHOST', 'localhost');
+define('DBNAME', 'bookcrm');
+define('DBUSER', 'ellen');
+define('DBPASS', 'mypass13');
+define('DBCONNSTRING','mysql:host=localhost;dbname=bookcrm');
+?>
